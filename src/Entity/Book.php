f47121870f136000d6eec279f7ffa8de45d08f11
mazen87 <mazen.karfoul87@hotmail.com>
2021-09-23 10:20:46 +0200

    add_functions_test_symfony

---
+++ src/Entity/Book.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ * @UniqueEntity(fields={"title"}, message=" This title is already used!")
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Assert\NotBlank
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="blob")
+     * @Assert\NotBlank
+     */
+    private $summary;
+
+    /**
+     * @ORM\Column(type="bigint")
+     * @Assert\NotBlank
+     * @Assert\Isbn(
+     *     type = "isbn13",
+     *     message = "This value is not valid."
+     * )
+     */
+    private $isbn13;
+
+    /**
+     * @ORM\Column(type="string", length=2083, nullable=true)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    public function setSummary($summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getIsbn13(): ?string
+    {
+        return $this->isbn13;
+    }
+
+    public function setIsbn13(string $isbn13): self
+    {
+        $this->isbn13 = $isbn13;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
---
+++ src/Form/BookType.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'constraints' => [
+                        new Regex([
+                            /**
+                             * accept lettres,numbers ,under score, dash, apostrophe
+                             */
+                            'pattern' => '/^([a-zA-Z0-9éèàùûêâôëç_\-\'])(?:[\s\'-]?(?1))*$/',
+                            'message' => 'This value is not valid. !'
+                        ])
+                    ],
+                    'attr' => [
+                        'placeholder' => 'Titre..',
+                        'class'       => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'author',
+                TextType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'constraints' => [
+                        new Regex([
+                            /**
+                             * accept lettres ,under score, dash, apostrophe
+                             */
+                            'pattern' => '/^([a-zA-Z-éèàùûêâôëç_\-\'])(?:[\s\'-]?(?1))*$/',
+                            'message' => 'This value is not valid. !'
+                        ])
+                    ],
+                    'attr' => [
+                        'placeholder' => 'Author..',
+                        'class'       => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'date',
+                DateType::class,
+                [
+                    'widget' => 'choice',
+                    'input'  => 'datetime_immutable'
+                ]
+            )
+            ->add(
+                'summary',
+                TextareaType::class,
+                [
+                    'label' => false,
+                    'required'    => false,
+                    'attr' => [
+                        'placeholder' => 'Summary...',
+                        'class'       => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'isbn13',
+                TextType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'attr' => [
+                        'placeholder' => 'ISBN..',
+                        'class'       => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'url',
+                UrlType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'attr' => [
+                        'placeholder' => "http://www.test_symfony@example.com",
+                        'class' => 'form-control'
+                    ]
+                ],
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
